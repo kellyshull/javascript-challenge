f78f00970809184c9ee9426d0493cf6d6f497866
Kelly Shull <kelly.t.shull@gmail.com>
2020-09-30 12:08:58 -0700

    finishing up level 1 app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,11 +21,44 @@ const tbody = d3.select("tbody");
 // });
 
 tableData.forEach(ufoSighting => {
-  console.log(ufoSighting);  
+  // console.log(ufoSighting);  
   let row = tbody.append("tr");
   Object.values(ufoSighting).forEach(value => {
  // Append a cell to the row for each value in the ufoSighting object
     var cell = row.append("td");
     cell.text(value);
   });
+});
+
+// USE date form in HTML document and search through date/time column to find rows that match user input
+
+//  select the button from html using d3 use #filter-btn 
+
+let submit = d3.select('#filter-btn');
+
+// complete the click handler for the click
+
+submit.on("click", function() {
+  // prevent refreshing the page
+  d3.event.preventDefault();
+
+  // select your input element from html 
+  var inputElement = d3.select("#datetime");
+
+  // get value of property of input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  // console.log(tableData);
+
+  // filter the data to date time
+  let filteredData = tableData.filter(instance => instance.datetime === inputValue);
+
+  console.log(filteredData);
+
+  // append the input value to the webpage / html using function above
+
+  
+
+  
 });
